3e48cd8269417445a0e6be503122293afe4e3f7f
Bastian Lampe <bastian.lampe@ika.rwth-aachen.de>
2026-06-30 14:05:45 +0200

    fix ROS setup sourcing with nounset enabled

--- docker/custom.sh
+++ docker/custom.sh
@@ -61,6 +61,8 @@ fi
 cd /docker-ros/ws
 git clone --branch jazzy-ika https://github.com/RaphvK/ros2_tracing.git src/ros2_tracing
 rosdep update && rosdep install -y -i --from-paths src/ros2_tracing/tracetools src/ros2_tracing/tracetools_launch
-source /opt/ros/${ROS_DISTRO}/setup.bash
+set +u
+source "/opt/ros/${ROS_DISTRO}/setup.bash"
+set -u
 colcon build --packages-up-to tracetools tracetools_launch --allow-overriding tracetools --allow-overriding tracetools_launch
 rm -r src/ros2_tracing log/ build/ 
